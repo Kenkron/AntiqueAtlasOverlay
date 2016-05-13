ac21d166858ffe6466a9a81be0055e0a83645131
Christopher Waugh <kenkron@gmail.com>
2016-05-13 15:10:14 -0400

    changing build version.

--- src/main/java/kenkron/antiqueatlasoverlay/AntiqueAtlasOverlayMod.java
+++ src/main/java/kenkron/antiqueatlasoverlay/AntiqueAtlasOverlayMod.java
@@ -12,7 +12,7 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 public class AntiqueAtlasOverlayMod
 {
     public static final String MODID = "AntiqueAtlasOverlay";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.2";
     
     AAORenderEventReceiver renderer;
     
